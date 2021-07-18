156dbf566ee0c6711dbfa328f3509f7d03e880d8
Helena <helenasleterkesto@gmail.com>
2021-07-18 15:53:18 +0200

    Add comments to markup, stylesheet and script file. When on save data from input is stored in localstorage

--- src/script.js
+++ src/script.js
@@ -1,17 +1,37 @@
 "use strict";
-
+//==================
+// VARIABLES
+//==================
 const notes = document.querySelector("#note");
 const btn = document.querySelector(".form-btn");
 let data;
 
+//==================
+// FUNCTIONS
+//==================
+
+/**
+ * Takes input from textarea and store in data variable
+ * @param {string} String   Textarea input value
+ */
 const inputHandler = () => {
     data = notes.value;
 };
 
+/**
+ * Takes data from inputHandler function and store
+ * in localstorage in key/value pair
+ * @param {function} Function   String as key/value pair
+ * @return {object}
+ */
 const onSaveHandler = (fn) => {
     localStorage.setItem("note", data);
     console.log(data);
 };
+
+//==================
+// EVENTHANDLERS
+//==================
 
 notes.addEventListener("input", inputHandler);
 btn.addEventListener("click", onSaveHandler);
